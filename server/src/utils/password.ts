d899617fb67b745b37dd9ee21418747701e71874
Pema Ngodup <pngodup123@gmail.com>
2026-02-02 00:26:30 +0000

    feat(utils): add custom ErrorResponse class

--- server/src/utils/password.ts
+++ server/src/utils/password.ts
@@ -5,6 +5,7 @@ export async function hashPassword(password: string) {
   return bcrypt.hash(password, 10);
 }
 
+// Verifying the hashed password
 export async function verifyHashPassword(password: string, hash: string) {
   return bcrypt.compare(password, hash);
 }
